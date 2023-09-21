ddb7c82ccafc6182ac87bfe76def7f2b384d2c8d
Ebad Uddin <ebad@aptech.com>
2023-09-21 15:26:09 +0500

    search functionality added

--- search/app.js
+++ search/app.js
@@ -1,22 +1,36 @@
 
+// Immediately invoke function
 ( async function(){
-
     let input = document.getElementById('input');
     let btn = document.getElementById('btn');
+    let list = document.getElementById('mylist');
     console.log("Please wait");
- await fetch('https://dummyjson.com/products').then(response => response.json()).then(result => console.log(result));
-
-
-
+ await fetch('https://dummyjson.com/products').then(response => response.json()).then(data => {
+    console.log(data.products)
+function displayData(result){
+    list.innerHTML = "";
+    result.forEach(e => {
+        // console.log(e);
+        
+   list.innerHTML += `
+   <li>${e.id}</li>
+   <li>${e.title}</li>
+   <li>${e.description}</li>
+   <li>${e.category}</li>
+   `
+});
+}
 function search(){
 let query = input.value;
-console.log(query);
+// console.log(query);
+let result = data.products.filter(function(product){
+    return (product.title.toLowerCase().includes(query) || product.category.toLowerCase().includes(query)) 
+})
+console.log(result);
+displayData(result)
 }
-
 btn.addEventListener('click', search)
-
-
-
+});
 }
 )
 ()
